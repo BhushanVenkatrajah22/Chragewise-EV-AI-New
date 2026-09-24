fa74d34f339b22491c2895d5390c68e648acce07
Bhushan Venkatrajah <bhushan.v.cse.2024@snsce.ac.in>
2026-09-24 14:56:08 +0530

    feat: initialize Express backend server with Socket.io, MongoDB connection, and email notification templates

--- node/index.js
+++ node/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dns').setServers(['8.8.8.8', '1.1.1.1']);
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
